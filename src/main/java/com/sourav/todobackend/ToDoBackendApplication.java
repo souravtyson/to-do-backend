75d832dcfcfef14f6a78a4cd0be4a04971fd77a8
sourav suman <souravtyson@gmail.com>
2025-06-02 00:23:16 +0530

    security config dependency added

--- src/main/java/com/sourav/todobackend/ToDoBackendApplication.java
+++ src/main/java/com/sourav/todobackend/ToDoBackendApplication.java
@@ -2,8 +2,10 @@ package com.sourav.todobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class ToDoBackendApplication {
 
     public static void main(String[] args) {
