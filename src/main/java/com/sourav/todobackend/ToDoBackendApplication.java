09bcecaa6187fee5d2af264b58faec536f279efe
souravtyson <souravtyson@gmail.com>
2025-06-02 12:47:02 +0000

    security config added but not in working condition

--- src/main/java/com/sourav/todobackend/ToDoBackendApplication.java
+++ src/main/java/com/sourav/todobackend/ToDoBackendApplication.java
@@ -2,10 +2,8 @@ package com.sourav.todobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity
 public class ToDoBackendApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/sourav/todobackend/configs/SecurityConfig.java
+++ src/main/java/com/sourav/todobackend/configs/SecurityConfig.java
@@ -3,24 +3,42 @@ package com.sourav.todobackend.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity.csrf()
-//                .disable()
-//                .authorizeHttpRequests()
-//                    .anyRequest()
-//                .authenticated();
-//
-//        httpSecurity.oauth2ResourceServer().jwt();
-//
-//        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        
+        httpSecurity.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/public/**").permitAll()
+                .anyRequest().authenticated()
+            ).oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt
+                    .jwtAuthenticationConverter(keycloakJwtConverter())
+                )
+            );
+
+        httpSecurity.cors(cor -> cor.configurationSource(req -> {
+            CorsConfiguration config = new CorsConfiguration();
+            config.addAllowedHeader("*");
+            config.addAllowedMethod("*");
+            config.addAllowedOrigin("*");
+            return config;
+        }));
+
+        httpSecurity.sessionManagement(session -> 
+        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return httpSecurity.build();
+    }
+
+    public JwtAuthenticationConverter keycloakJwtConverter() {
+        
     }
 }
